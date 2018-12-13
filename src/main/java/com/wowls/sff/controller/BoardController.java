fb8add0d78cf9c169fa5f9d96e1c104ed78919cc
woo <dntnrmsss@naver.com>
2018-12-13 22:10:28 +0900

    server port 7701

--- src/main/java/com/wowls/sff/controller/BoardController.java
+++ src/main/java/com/wowls/sff/controller/BoardController.java
@@ -2,7 +2,6 @@ package com.wowls.sff.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/boards*")
 public class BoardController {
 
-	@Autowired
+//	@Autowired
 //	private FoodMapService foodMapService;
 	
 	@PostMapping
--- src/main/java/com/wowls/sff/controller/FavoriteController.java
+++ src/main/java/com/wowls/sff/controller/FavoriteController.java
@@ -3,7 +3,6 @@ package com.wowls.sff.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/favorite*")
 public class FavoriteController {
 
-	@Autowired
+//	@Autowired
 //	private FoodMapService foodMapService;
 	
 	@PostMapping("/{storeId}")
